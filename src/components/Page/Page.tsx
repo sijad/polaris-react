9d251bd7b959ed8bcb2784faac1c12e883db80fa
Tim Layton <tim.layton@shopify.com>
2019-08-15 16:49:24 -0700

    remove app bridge from page

--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -1,16 +1,6 @@
 import React from 'react';
-import {
-  Button as AppBridgeButton,
-  TitleBar as AppBridgeTitleBar,
-} from '@shopify/app-bridge/actions';
-import isEqual from 'lodash/isEqual';
 
 import {classNames} from '../../utilities/css';
-import {
-  transformActions,
-  generateRedirect,
-} from '../../utilities/app-bridge-transformers';
-import {pick} from '../../utilities/pick';
 import {
   withAppProvider,
   WithAppProviderProps,
@@ -26,13 +16,6 @@ export interface Props extends HeaderProps {
   fullWidth?: boolean;
   /** Decreases the maximum layout width. Intended for single-column layouts */
   narrowWidth?: boolean;
-  /**
-   * Force render in page and do not delegate to the app bridge TitleBar action
-   * @default false
-   * @embeddedAppOnly
-   * @see {@link https://polaris.shopify.com/components/structure/page#section-use-in-an-embedded-application|Shopify Page Component docs}
-   * */
-  forceRender?: boolean;
 }
 
 export interface DeprecatedProps {
@@ -44,61 +27,7 @@ export interface DeprecatedProps {
 
 export type ComposedProps = Props & DeprecatedProps & WithAppProviderProps;
 
-const APP_BRIDGE_PROPS: (keyof Props)[] = [
-  'title',
-  'breadcrumbs',
-  'secondaryActions',
-  'actionGroups',
-  'primaryAction',
-];
-
 class Page extends React.PureComponent<ComposedProps, never> {
-  private titlebar: AppBridgeTitleBar.TitleBar | undefined;
-
-  componentDidMount() {
-    if (this.delegateToAppbridge === false || !this.props.polaris.appBridge) {
-      return;
-    }
-
-    const transformedProps = this.transformProps();
-    if (!transformedProps) return;
-
-    // eslint-disable-next-line no-console
-    console.warn(
-      'Deprecation: Using `Page` to render an embedded app title bar is deprecated and will be removed in v5.0. Use `TitleBar` from `@shopify/app-bridge-react` instead: https://help.shopify.com/en/api/embedded-apps/app-bridge/react-components/titlebar',
-    );
-
-    this.titlebar = AppBridgeTitleBar.create(
-      this.props.polaris.appBridge,
-      transformedProps,
-    );
-  }
-
-  componentDidUpdate(prevProps: ComposedProps) {
-    if (this.titlebar == null || this.delegateToAppbridge === false) {
-      return;
-    }
-
-    const prevAppBridgeProps = pick(prevProps, APP_BRIDGE_PROPS);
-    const currentAppBridgeProps = pick(this.props, APP_BRIDGE_PROPS);
-
-    if (!isEqual(prevAppBridgeProps, currentAppBridgeProps)) {
-      const transformedProps = this.transformProps();
-      if (!transformedProps) return;
-
-      this.titlebar.unsubscribe();
-      this.titlebar.set(transformedProps);
-    }
-  }
-
-  componentWillUnmount() {
-    if (this.titlebar == null || this.delegateToAppbridge === false) {
-      return;
-    }
-
-    this.titlebar.unsubscribe();
-  }
-
   render() {
     const {
       children,
@@ -122,9 +51,7 @@ class Page extends React.PureComponent<ComposedProps, never> {
     );
 
     const headerMarkup =
-      this.delegateToAppbridge || this.hasHeaderContent() === false ? null : (
-        <Header {...rest} />
-      );
+      this.hasHeaderContent() === false ? null : <Header {...rest} />;
 
     return (
       <div className={className}>
@@ -132,15 +59,6 @@ class Page extends React.PureComponent<ComposedProps, never> {
         <div className={styles.Content}>{children}</div>
       </div>
     );
-  }
-
-  private get delegateToAppbridge(): boolean {
-    const {
-      polaris: {appBridge},
-      forceRender = false,
-    } = this.props;
-
-    return appBridge != null && forceRender === false;
   }
 
   private hasHeaderContent(): boolean {
@@ -159,47 +77,6 @@ class Page extends React.PureComponent<ComposedProps, never> {
       (actionGroups != null && actionGroups.length > 0) ||
       (breadcrumbs != null && breadcrumbs.length > 0)
     );
-  }
-
-  private transformProps(): AppBridgeTitleBar.Options | void {
-    const {appBridge} = this.props.polaris;
-    if (!appBridge) return;
-    const {title, primaryAction, secondaryActions, actionGroups} = this.props;
-
-    return {
-      title,
-      buttons: transformActions(appBridge, {
-        primaryAction,
-        secondaryActions,
-        actionGroups,
-      }),
-      breadcrumbs: this.transformBreadcrumbs(),
-    };
-  }
-
-  private transformBreadcrumbs(): AppBridgeButton.Button | undefined {
-    const {appBridge} = this.props.polaris;
-    if (!appBridge) return;
-    const {breadcrumbs} = this.props;
-
-    if (breadcrumbs != null && breadcrumbs.length > 0) {
-      const breadcrumb = breadcrumbs[breadcrumbs.length - 1];
-      const button = AppBridgeButton.create(appBridge, {
-        label: breadcrumb.content || '',
-      });
-
-      const callback = !('url' in breadcrumb)
-        ? breadcrumb.onAction
-        : generateRedirect(appBridge, breadcrumb.url, breadcrumb.target);
-
-      if (callback != null) {
-        button.subscribe(AppBridgeButton.Action.CLICK, callback);
-      }
-
-      return button;
-    } else {
-      return undefined;
-    }
   }
 }
 
